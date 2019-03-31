9ec551c18132018f05ec2b8d647b39a489b8b585
wcollins37 <wcollins37@gatech.edu>
2019-03-31 02:50:13 -0400

    Starting API integration, getting no requests from server

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
 Vue.component('gchart', VueGoogleCharts.GChart);
 
-var app = new Vue({})
+var app = new Vue({
+    el: '#app',
+
+    data () {
+        return {
+            apiData: []
+        }
+    },
+
+    mounted () {
+        axios
+            .get('http://127.0.0.1/departments')
+            .then(function(response){
+                console.log(response);
+                this.apiData = response.data.results;
+            })
+    }
+
+})
