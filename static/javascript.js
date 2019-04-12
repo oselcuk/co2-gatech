73062390505683551e19dc4ae56f55c2dfed6d10
wcollins37 <wcollins37@gatech.edu>
2019-04-12 04:21:26 -0400

    All charts added; date selection and general visual cleanup still needed

--- static/javascript.js
+++ static/javascript.js
@@ -6,6 +6,7 @@ var app = new Vue({
     data () {
         return {
             apiData: [],
+            emitterData: [],
             chartData: [
                 ['Organization', 'Total Emissions'],
             ],
@@ -18,36 +19,67 @@ var app = new Vue({
     },
 
     mounted () {
-        return axios
-            .get('https://co2-gatech.appspot.com/api/departments')
+        return axios.all([
+            axios.get('https://co2-gatech.appspot.com/api/departments')
             .then((response) => {
                 console.log(response.data.result);
                 this.apiData = response.data.result;
             })
+            .catch(error => console.log(error)),
+            axios.get('https://co2-gatech.appspot.com/api/top_emitters')
+            .then((response) => {
+                console.log(response.data.result);
+                this.emitterData = response.data.result;
+            })
             .catch(error => console.log(error))
+        ])
     },
 
     computed: {
         pieChartData: function() {
             const pieData = this.chartData;
             for (let i = 0; i < this.apiData.length; i += 1) {
-                const point = [this.apiData[i].name, this.apiData[i].totalEmissions];
+                const point = [this.apiData[i].name, this.apiData[i].total_emissions];
                 pieData.push(point);
             }
 
             return pieData;
         },
 
-        barChartData: function() {
-            const barData = [
+        columnChartData: function() {
+            const columnData = [
                 ['Year', 'Total Overall Emissions']
             ];
             var total = 0;
             for (let i = 0; i < this.apiData.length; i += 1) {
-                total += this.apiData[i].totalEmissions;
+                total = total + this.apiData[i].total_emissions;
             }
-            barData.push(['2018', total]);
+            columnData.push(['2018', total]);
+            return columnData;
+        },
+
+        barChartData: function() {
+            const barData = [
+                ['Department', 'Trip Mileage']
+            ];
+            for (let i = 0; i < this.apiData.length; i += 1) {
+                const point = [this.apiData[i].name, this.apiData[i].total_distance];
+                barData.push(point);
+            }
+
             return barData;
+        },
+
+        lineChartData: function() {
+            const lineData = [
+                ['Emissions', 'Rank']
+            ];
+            for (let i = 0; i < this.emitterData.length; i += 1) {
+                const point = [this.emitterData[i].totalEmissions, this.emitterData[i].rank];
+                lineData.push(point);
+            }
+
+            return lineData;
         }
     }
 
